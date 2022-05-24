f3928cddee47dc0887c5d8cc88624430c09bfea8
RivkaWal <rw0533111729@gmail.com>
2022-05-24 21:31:11 +0300

    add select tag to category textfield

--- src/recipts.js
+++ src/recipts.js
@@ -1,230 +1,4 @@
-// import * as React from 'react';
-// import { styled } from '@mui/material/styles';
-// import Paper from '@mui/material/Paper';
-// import axios from 'axios';
-// import { useForm } from "react-hook-form";
-// import {
-//     DataGrid, GridCellEditStopParams,
-//     GridCellEditStopReasons,
-//     MuiEvent,
-// } from '@mui/x-data-grid';
-// import createCache from '@emotion/cache';
-// import { CacheProvider } from '@emotion/react';
-// import rtlPlugin from 'stylis-plugin-rtl';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import { prefixer } from 'stylis';
 
-
-// import {
-//     Box,
-//     Button,
-//     ButtonBase,
-//     FormControl,
-//     Input,
-//     InputAdornment,
-//     InputLabel,
-//     TextField,
-// } from "@mui/material";
-// import { useState, useEffect } from "react";
-
-// const Item = styled(Paper)(({ theme }) => ({
-//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//     ...theme.typography.body2,
-//     padding: theme.spacing(1),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-// }));
-
-// export default function ColumnsGrid() {
-//     const { register, handleSubmit, setError } = useForm();
-
-//     const getColumns = () => {
-//         return [
-//             { field: 'nameProduct', headerName: 'שם המוצר', width: 70 },
-//             { field: 'amount', headerName: 'כמות', width: 70, type: 'number', },
-//             // { width: 300 },
-//             { field: 'sumProduct', headerName: ' מחיר סופי', width: 430 },
-//         ];
-//     }
-//     const [rows, setRows] = useState([]);
-//     const [columns, setColumns] = useState(getColumns);
-//     const [AllReceipt, setAllRecipt] = useState({
-//         id: 1,
-//         dateReceipt: "",
-//         nameShop: "",
-//         numCompany: "",
-//         totalSum: "",
-//         myUser: "",
-//         category: "",
-
-//         products: [{
-//             id: 1, key: 1, nameProduct: "לחם", amount: "3", sumProduct: "15", editable: true,
-//         },
-//         {
-//             id: 2, key: 2, nameProduct: "חלב", amount: "2", sumProduct: "10", editable: true,
-//         },
-//         {
-//             id: 3, key: 3, nameProduct: "מילקי", amount: "8", sumProduct: "16", editable: true,
-//         }
-//         ]
-//     })
-//     useEffect(() => {
-//         async function getResults() {
-//             //  const result = await axios.get("https://localhost:44391/api/SaveReceipt");
-//             // setRows(result.data);
-//             setRows(AllReceipt.products);
-//         }
-//         getResults()
-//     }, [])
-
-//     async function addReceipt() {
-//         setAllRecipt({
-//             // dateReceipt: 
-//             // nameShop: "register.nameShop",
-//             // numCompany: "register.numCompany",
-//             // totalSum: "register.totalSum",
-//             // myUser: "",
-//             // category: "register.category",
-//             // products: [{
-//             //     nameProduct: "", amount: "", sumProduct: ""
-//             // },]
-//         });
-//         console.log(AllReceipt);
-//         axios.post(`https://localhost:44391/api/SaveReceipt`, AllReceipt)
-//             .then(res => {
-//                 console.log(res);
-//                 console.log(res.data);
-//             });
-
-
-//     }
-//     const dateReceiptRef = React.createRef();
-//     const nameShopRef = React.createRef();
-//     const numCompanyRef = React.createRef();
-//     const totalSumRef = React.createRef();
-//     const categoryRef = React.createRef();
-
-
-
-
-//     const submit = () => {
-
-//         setAllRecipt({ dateReceipt: dateReceiptRef.current.value, nameShop: nameShopRef.current.value, numCompany: numCompanyRef.current.value })
-
-//     }
-//     const theme = createTheme({
-//         direction: 'rtl',
-//         palette: {
-//             primary: {
-//                 main: 'rgb(243, 26, 73)'
-//             }
-//         }
-//     });
-//     const cacheRtl = createCache({
-//         key: 'muirtl',
-//         stylisPlugins: [prefixer, rtlPlugin],
-//     });
-//     return (
-//         <div className="stepsWrapper">
-//             <CacheProvider value={cacheRtl}>
-//                 <ThemeProvider theme={theme}>
-
-//                     <Box component="form"
-//                         sx={{
-//                             flexGrow: 1,
-//                             '& .MuiTextField-root': { m: 1, width: '25ch' },
-//                         }}
-//                         noValidate
-//                         autoComplete="off">
-//                         <Item sx={{ padding: 20, height: "100vh" }}><div>
-
-//                             <TextField
-//                                 required
-//                                 id="standard-required"
-//                                 label="תאריך"
-//                                 defaultValue={AllReceipt.dateReceipt}
-//                                 variant="standard"
-//                             />
-//                             <TextField
-//                                 required
-//                                 id="standard-required"
-//                                 label="שם חנות"
-//                                 defaultValue={AllReceipt.nameShop}
-//                                 variant="standard"
-//                             // {...register("nameShop")}
-//                             />
-//                             <TextField
-//                                 required
-//                                 id="standard-required"
-//                                 label="עוסק מורשה"
-//                                 defaultValue={AllReceipt.numCompany}
-//                                 variant="standard"
-//                             // ref
-//                             // {...register("numCompany")}
-//                             />
-//                             <TextField
-//                                 required
-//                                 id="standard-required"
-//                                 label="מחיר סופי"
-//                                 defaultValue={AllReceipt.totalSum}
-//                                 variant="standard"
-//                             // {...register("totalSum")}
-//                             />
-
-//                             <TextField
-//                                 required
-//                                 id="standard-required"
-//                                 label="שם משתמש"
-//                                 defaultValue={AllReceipt.myUser}
-//                                 variant="standard"
-//                             // {...register("myUser")}
-//                             />
-//                             <TextField
-//                                 required
-//                                 id="standard-required"
-//                                 label="קטגוריה"
-//                                 // type=""
-//                                 defaultValue={AllReceipt.category}
-//                                 variant="standard"
-//                             // {...register("category")}
-//                             />
-//                             <div style={{ height: 400, width: '100%' }}>
-//                                 {/* <DataGrid
-//                                     rows={rows}
-//                                     columns={columns}
-//                                     editable={true}
-
-//                                 /> */}
-//                                 <DataGrid
-//                                     rows={rows}
-//                                     columns={columns}
-//                                     experimentalFeatures={{ newEditingApi: true }}
-//                                     // onCellEditStop={(params: GridCellEditStopParams, event: MuiEvent) => {
-//                                     //     if (params.reason === GridCellEditStopReasons.cellFocusOut) {
-//                                     //         event.defaultMuiPrevented = true;
-//                                     //     }
-//                                     // }}
-//                                 />
-//                             </div>
-//                             <Button onClick={addReceipt}>save</Button>
-
-//                         </div>
-//                         </Item>
-//                     </Box>
-//                 </ThemeProvider>
-//             </CacheProvider>
-//         </div>
-//     );
-// }
-
-//     // async function addReceipt (){
-
-//     //     var promise = axios.post("https://localhost:44391/api/SaveReceipt",AllReceipt);
-//     //     var res = promise;
-//     //     message = (await res).status;
-//     //     console.log("success");
-//     //     // this.setState({});
-//     //   };
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
@@ -236,17 +10,22 @@ import { CacheProvider } from '@emotion/react';
 import rtlPlugin from 'stylis-plugin-rtl';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { prefixer } from 'stylis';
-
+import Box from '@mui/material/Box';
+// import InputLabel from '@mui/material/InputLabel';
+// import FormControl from '@mui/material/FormControl';
+// import NativeSelect from '@mui/material/NativeSelect';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
 
 import {
-    Box,
+    //Box,
     Button,
     ButtonBase,
-    FormControl,
+    //FormControl,
     Input,
     InputAdornment,
-    InputLabel,
-    TextField,
+    // InputLabel,
+    //TextField,
 } from "@mui/material";
 import { useState, useEffect, useRef } from "react";
 
@@ -328,6 +107,41 @@ export default function ColumnsGrid() {
         key: 'muirtl',
         stylisPlugins: [prefixer, rtlPlugin],
     });
+
+
+    const currencies = [
+        {
+            value: 'food',
+            label: 'מזון',
+        },
+        {
+            value: 'cloths',
+            label: 'בגוד',
+        },
+        {
+            value: 'shoes',
+            label: 'הנעלה',
+        },
+        {
+            value: 'transportation',
+            label: 'תחבורה',
+        },
+        {
+            value: 'study pay',
+            label: 'שכר לימוד',
+        },
+        {
+            value: 'property tax',
+            label: 'ארנונה',
+        },
+
+    ];
+    const [currency, setCurrency] = React.useState('food');
+
+    const handleChange = (event) => {
+        setCurrency(event.target.value);
+    };
+
     return (
         <div className="stepsWrapper">
             <CacheProvider value={cacheRtl}>
@@ -384,21 +198,34 @@ export default function ColumnsGrid() {
                                 variant="standard"
                                 onChange={(e) => setAllRecipt({ ...AllReceipt, myUser: e.target.value })}
                             />
+
                             <TextField
                                 required
-                                id="standard-required"
+                                id="standard-select-currency"
+                                select
                                 label="קטגוריה"
-                                // type=""
-                                defaultValue={AllReceipt.category}
+                                type="NativeSelect"
+                                //type="outlined"
+                                // defaultValue={AllReceipt.category}
+                                // value={currency}
+                                onChange={handleChange}
+                                // helperText="Please select your currency"
                                 variant="standard"
                                 onChange={(e) => setAllRecipt({ ...AllReceipt, category: e.target.value })}
-                            />
+                            >
+                                {currencies.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+
                             <div style={{ height: 400, width: '100%' }}>
                                 <DataGrid
                                     rows={rows}
                                     columns={columns}
 
-                                   
+
                                 />
                             </div>
                             <Button onClick={addReceipt}>save</Button>
@@ -408,7 +235,7 @@ export default function ColumnsGrid() {
                     </Box>
                 </ThemeProvider>
             </CacheProvider>
-        </div>
+        </div >
     );
 }
 
@@ -439,6 +266,6 @@ export default function ColumnsGrid() {
     //     );
     //     $(".table tbody").append(appRow);
     //   }
-    
+
     //   render() {
     //     return (

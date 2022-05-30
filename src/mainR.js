3424dcd5d7b19426e53e5cf53fb152cfd356aeaf
racheliSHu <rachel33052@gmail.com>
2022-05-30 20:02:26 +0300

    button

--- src/mainR.js
+++ src/mainR.js
@@ -1,14 +1,13 @@
 import { useNavigate } from "react-router-dom";
-import ColumnsGrid from './recipts';
 import Button from '@mui/material/Button';
 
 export default function Main() {
     let navigate = useNavigate()
     return (
-        <div className="splitScreen" >
-            <Button onClick={() => navigate("/recipts")}>העלאת חשבונית</Button>
+        <div className="splitScreen2" >
+            <Button  onClick={() => navigate("/recipts")}>העלאת חשבונית</Button>
             <Button onClick={() => navigate("/recipt")}>להוספת חשבונית ידנית</Button>
-            {/* <Button onClick={() => navigate("/past")}>לחשבוניות קודמות</Button> */}
+            <Button onClick={() => navigate("/past")}>לחשבוניות קודמות</Button> 
         </div >
     );
 }
